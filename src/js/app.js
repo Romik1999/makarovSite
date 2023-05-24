2ff4bd981ecccb1c5201ddefb28f976524c5ec80
Romik1999 <usankin.roma@gmail.com>
2023-05-25 01:51:01 +0400

    work with form

--- src/js/app.js
+++ src/js/app.js
@@ -46,7 +46,39 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 
+    let radioInputs = document.querySelectorAll(".form-confirm__radio");
 
+    if (radioInputs) {
+        console.log(radioInputs);
+        radioInputs.forEach(radioInput => {
+            radioInput.addEventListener('input', function () {
+                let val = this.value;
+                console.log(val);
+                if (val === 'с радостью приду!'){
+                    console.log(123);
+                    document.querySelector('.form__alcohol').classList.toggle('active')
+                    console.log(9999);
+                }
+            });
+        })
+    }
+
+    //ползунок
+    const rangeInput = document.querySelector(".range__input"),
+        rangeTrack = document.querySelector(".range__track");
+
+    rangeInput.addEventListener('input', function () {
+        let val = +this.value,
+            min = +this.getAttribute('min'),
+            max = +this.getAttribute('max'),
+            step = +this.getAttribute('step'),
+            position = 100 / (max - step) * (val - step);
+
+        rangeTrack.style.width = `${position}%`;
+    });
+
+
+    // таймер
     let countDownDate = new Date("August 19, 2023 17:00:00").getTime();
 
     let x = setInterval(function () {
